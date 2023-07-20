f30ddb0e910638d8f2377ca38ca0d9403e29c681
sachinmaile <maile.sachin16@gmail.com>
2023-07-20 18:56:33 +0530

    Demo project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,18 @@
-import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { Link } from 'react-router-dom'
 
-const NavbarMenu = ()=>{
+//get button name and path for evaery component
+const Navbar = (props) => {
+
   return (
-    <>
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/AllProducts">Album</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/AllProducts">Show Album</Nav.Link>
-            <Nav.Link href="/AddProduct">Add Album</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+    <div className='navbar'>
+      <h2 onClick={() => props.handleClick(false)}>
+        <span className='brand-first-half'>ALBUMS</span>
+        <span className='brand-last-half'>LIST</span>
+      </h2>
+      <Link to={props.path}><button>{props.page}</button></Link>
+    </div>
+  )
 }
 
-export default NavbarMenu; 
+export default Navbar
