5b11b38b7ed1e868712536af7158775800dce4ad
sachinmaile <maile.sachin16@gmail.com>
2023-07-20 19:35:06 +0530

    Styling

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
 //get album from album list and show using html css and js
 const List = (props) => {
@@ -7,8 +8,8 @@ const List = (props) => {
     <div className='list'>
       <h3>{props.album.title}</h3>
       <div className='button-group'>
-        <Link to="/update-album"><button className="update-btn" onClick={() => props.setUpdateAlbum(props.album)}>Update</button></Link>
-        <button className='delete-btn' onClick={() => props.deleteAlbumFromList(props.album.id)}>Delete</button>
+        <Link to="/update-album"><Button variant="warning" onClick={() => props.setUpdateAlbum(props.album)}>Update</Button></Link>
+        <Button variant='danger' onClick={() => props.deleteAlbumFromList(props.album.id)}>Delete</Button>
       </div>
     </div>
   )
